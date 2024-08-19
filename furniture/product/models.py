e1737a6c4209053377b07224cba83eac5f3e874c
seokjoon911 <seokjoon911@gmail.com>
2024-08-19 16:43:34 +0900

    fix user, product

--- furniture/product/models.py
+++ furniture/product/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
+from django.conf import settings
 
-# Create your models here.
+class Product(models.Model):
+    prod_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    content = models.TextField() #글 내용
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, db_column='user', to_field='nickname')
+    image = models.ImageField(null=True)  # 피드 이미지
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'product'
