e14323d4b019a731890a117f93edb100eecebbf3
seokjoon911 <seokjoon911@gmail.com>
2024-09-01 18:17:34 +0900

    fix cart add, etc.

--- furniture/cart/models.py
+++ furniture/cart/models.py
@@ -7,8 +7,8 @@ class Cart(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, db_column='user', to_field='nickname')
     prod_id = models.ForeignKey('product.Product', on_delete=models.CASCADE, db_column='prod_id', to_field='prod_id')
-    quantity = models.IntegerField(default=1)
-    created_at = models.DateTimeField(auto_now_add=True)
+    quantity = models.IntegerField(default=1) # 수량
+    created_at = models.DateTimeField(auto_now_add=True) #장바구니 담은 시간(최대 90까지 담을 수 있게 하기 위함)
 
     class Meta:
         db_table = 'cart'
--- furniture/cart/views.py
+++ furniture/cart/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes, authentication_classes, parser_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.parsers import MultiPartParser
+from rest_framework.response import Response
+from cart.serializers import CartSerializer
+from drf_yasg.utils import swagger_auto_schema
 
-# Create your views here.
+@swagger_auto_schema(
+    method='post',
+    operation_id='제품 장바구니 등록',
+    operation_description='제품을 장바구니에 등록합니다.',
+    tags=['Cart'],
+    request_body=CartSerializer,
+)
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([JWTAuthentication]) # JWT토큰 확인
+def cart_add(request):
+    serializer = CartSerializer(data=request.data)
+
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
