23ead3994ca6d98ecaa5b07f9d07f9ac56dcf978
seokjoon911 <seokjoon911@gmail.com>
2024-09-20 20:26:49 +0900

    fix product models.py

--- furniture/product/models.py
+++ furniture/product/models.py
@@ -5,11 +5,11 @@ from menu.models import SubCategory
 
 class Product(models.Model):
     prod_id = models.CharField(max_length=36, primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=100) #제목
-    content = models.TextField() #내용
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, db_column='user', to_field='nickname')
-#    category = models.ForeignKey(SubCategory, on_delete=models.CASCADE, db_column='id', to_field='id')
+    category_id = models.ForeignKey(SubCategory, on_delete=models.CASCADE, db_column='category_id', to_field='id')
+    title = models.CharField(max_length=100)  # 제목
+    content = models.TextField()  # 내용
     image = models.ImageField(null=True)  # 피드 이미지
     price = models.IntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
